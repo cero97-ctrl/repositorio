102e75538707eb8ca2654b1edd87a2271647f7ef
CESAR RODRIGUEZ <cero97@yahoo.com>
2025-11-13 07:36:25 -0400

    REPOSITORIO

--- commit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e # Salir inmediatamente si un comando falla
-
-mensaje=${1:-"Actualización"}
-branch=$(git rev-parse --abbrev-ref HEAD)
-git add .
-git commit -m "$mensaje"
-git push origin "$branch"
